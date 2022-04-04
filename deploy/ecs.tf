f1fbff3126be83a30b9c2b3594d38f7f94749478
Tobi Oladipo <Tobi.Oladipo@hilti.com>
2022-04-04 19:19:21 +0200

    centralizing the data elements

--- deploy/ecs.tf
+++ deploy/ecs.tf
@@ -1,24 +1,3 @@
-# Pulling in existing network data
-data "aws_vpc" "main" {
-  id = var.vpc_id
-}
-
-data "aws_subnet" "private-1" {
-  id = var.private1
-}
-
-data "aws_subnet" "private-2" {
-  id = var.private2
-}
-
-data "aws_subnet" "public-1" {
-  id = var.public1
-}
-
-data "aws_subnet" "public-2" {
-  id = var.public2
-}
-
 resource "aws_ecs_cluster" "main" {
   name = "${local.prefix}-cluster"
 
